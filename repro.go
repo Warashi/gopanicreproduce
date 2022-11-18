a0ea474fc37885e157b84846fdeec3eac26fa020
Shinnosuke Sawada <6warashi9@gmail.com>
2022-11-18 21:18:57 +0900

    reproduced panic

---
+++ repro.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/gostaticanalysis/testutil"
+	_ "golang.org/x/tools/go/analysis"
+	_ "golang.org/x/tools/go/analysis/analysistest"
+	_ "golang.org/x/tools/go/analysis/passes/buildssa"
+)
